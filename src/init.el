4a944f07bc99de0baa7f61542d6d63691fc74e55
Tomek Wałkuski <ja@jestem.tw>
2014-10-21 20:37:11 +0200

    init.el cleanup

--- src/init.el
+++ src/init.el
@@ -1,44 +1,37 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 (package-initialize)
 
 (setq package-list
-      '(cider clojure-mode color-theme-solarized god-mode
-	      highlight-parentheses rainbow-delimiters
-	      ruby-mode smartparens))
+      '(clojure-mode highlight-parentheses rainbow-delimiters
+		     smartparens ruby-mode solarized-theme))
 
 (dolist (package package-list)
   (unless (package-installed-p package)
     (package-refresh-contents)
     (package-install package)))
 
-; cider
-(add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
-(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
-
 ; color-theme
 (load-theme 'solarized-dark t)
 
-; highlight-parentheses
+; smartparens
+(smartparens-global-mode t)
+
+;; highlight-parentheses
 (define-globalized-minor-mode global-highlight-parentheses-mode
                               highlight-parentheses-mode
                               (lambda ()
                                 (highlight-parentheses-mode t)))
 (global-highlight-parentheses-mode t)
 
-; linum
+;; rainbow-delimiters
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;; various
 (global-linum-mode t)
-
-; rainbow-delimiters
-(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-
-; smartparens
-(smartparens-global-mode t)
-
-; various
 (tool-bar-mode -1)
 (setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
