cf1c4ee372b563473f52285f096dfeed164c7ba8
Tomek Wałkuski <ja@jestem.tw>
2014-10-26 18:35:20 +0100

    Install rspec-mode

--- src/init.el
+++ src/init.el
@@ -8,8 +8,8 @@
 (setq package-list
       '(ag clojure-mode company fill-column-indicator flx-ido
            highlight-parentheses magit monokai-theme neotree projectile
-           rainbow-delimiters smartparens ruby-mode sane-term web-mode
-           whitespace-cleanup-mode))
+           rainbow-delimiters rspec-mode smartparens ruby-mode sane-term
+           web-mode whitespace-cleanup-mode))
 
 (dolist (package package-list)
   (unless (package-installed-p package)
@@ -78,3 +78,15 @@
 (global-set-key (kbd "C-x <left>") 'windmove-left)
 (global-set-key (kbd "C-x <right>") 'windmove-right)
 (global-set-key (kbd "M-n") 'neotree-toggle)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((encoding . utf-8)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
