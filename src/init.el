cdaf84ab421ed0ad28081b9f361b8684c425b0ec
Tomek Wałkuski <ja@jestem.tw>
2015-06-16 08:42:27 +0200

    Enable web-mode for scss and json

--- src/init.el
+++ src/init.el
@@ -165,6 +165,8 @@
                (setq web-mode-css-indent-offset 2)
                (setq web-mode-code-indent-offset 2)
                (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+               (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+               (add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
                (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))))
 
 (use-package whitespace-cleanup-mode
