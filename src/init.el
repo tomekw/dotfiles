fc74a55e8e1a06a07fbc57b49b452fa839f6eb18
Tomek Wałkuski <ja@jestem.tw>
2015-11-25 09:00:49 +0100

    Set use-package-always-ensure to true, no need for ':ensure t'

--- src/init.el
+++ src/init.el
@@ -37,6 +37,7 @@
 (setq mac-command-modifier 'meta)
 (setq ns-right-alternate-modifier nil)
 (setq require-final-newline t)
+(setq use-package-always-ensure t)
 
 (bind-key "C-M-f" 'toggle-frame-fullscreen)
 (bind-key "C-x <up>" 'windmove-up)
@@ -51,55 +52,43 @@
 (custom-set-faces
  '(font-lock-type-face ((t (:foreground "turquoise3")))))
 
-(use-package alchemist
-  :ensure t)
+(use-package alchemist)
 
-(use-package cider
-  :ensure t)
+(use-package cider)
 
-(use-package clojure-mode
-  :ensure t)
+(use-package clojure-mode)
 
 (use-package company
-  :ensure t
   :diminish company-mode
   :init (add-hook 'after-init-hook 'global-company-mode))
 
-(use-package elixir-mode
-  :ensure t)
+(use-package elixir-mode)
 
 (use-package evil-leader
-  :ensure t
   :init (progn (global-evil-leader-mode)
                (evil-leader/set-leader ",")))
 
 (use-package evil
-  :ensure t
   :init (evil-mode 1))
 
 (use-package farmhouse-theme
-  :ensure t
   :init (progn (load-theme 'farmhouse-dark t)
                (set-frame-font "Monaco 12")))
 
 (use-package helm
-  :ensure t
   :diminish helm-mode
   :init (helm-mode 1)
   :bind ("M-x" . helm-M-x))
 
 (use-package helm-ag
-  :ensure t
   :init (defalias 'ag 'projectile-helm-ag))
 
 (use-package helm-projectile
-  :ensure t
   :init (defun projectile-helm-ag ()
           (interactive)
           (helm-ag (projectile-project-root))))
 
 (use-package highlight-parentheses
-  :ensure t
   :init (progn (define-globalized-minor-mode global-highlight-parentheses-mode
                  highlight-parentheses-mode
                  (lambda ()
@@ -107,15 +96,12 @@
                (global-highlight-parentheses-mode t)))
 
 (use-package magit
-  :ensure t
   :bind ("C-x g" . magit-status))
 
 (use-package markdown-mode
-  :ensure t
   :config (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode)))
 
 (use-package projectile
-  :ensure t
   :diminish projectile-mode
   :init (progn (setq inhibit-startup-screen t)
                (projectile-global-mode)
@@ -124,50 +110,39 @@
                (helm-projectile-on)))
 
 (use-package projectile-rails
-  :ensure t
   :init (add-hook 'projectile-mode-hook 'projectile-rails-on))
 
-(use-package puppet-mode
-  :ensure t)
+(use-package puppet-mode)
 
 (use-package rainbow-delimiters
-  :ensure t
   :init (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 (use-package rbenv
-  :ensure t
   :init (progn (setq rbenv-show-active-ruby-in-modeline nil)
                (global-rbenv-mode)))
 
 (use-package recentf
-  :ensure t
   :init (progn (recentf-mode 1)
                (add-to-list 'recentf-exclude ".git")
                (add-to-list 'recentf-exclude "elpa"))
   :bind ("C-x C-r" . helm-recentf))
 
-(use-package rspec-mode
-  :ensure t)
+(use-package rspec-mode)
 
-(use-package ruby-hash-syntax
-  :ensure t)
+(use-package ruby-hash-syntax)
 
 (use-package ruby-mode
-  :ensure t
   :init (setq ruby-insert-encoding-magic-comment nil))
 
 (use-package sane-term
-  :ensure t
   :init (setq sane-term-shell-command "/usr/local/bin/zsh")
   :bind ("M-t" . sane-term))
 
 (use-package scss-mode
-  :ensure t
   :init (progn (setq scss-compile-at-save nil)
                (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))))
 
 (use-package smartparens
-  :ensure t
   :init (progn (smartparens-global-mode t)
                (show-smartparens-global-mode t)
                (require 'smartparens-config)
@@ -181,7 +156,6 @@
                                 :actions '(insert navigate)))))
 
 (use-package web-mode
-  :ensure t
   :init (progn (setq web-mode-markup-indent-offset 2)
                (setq web-mode-css-indent-offset 2)
                (setq web-mode-code-indent-offset 2)
@@ -191,8 +165,6 @@
                (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))))
 
 (use-package whitespace-cleanup-mode
-  :ensure t
   :init (global-whitespace-cleanup-mode t))
 
-(use-package yaml-tomato
-  :ensure t)
+(use-package yaml-tomato)
